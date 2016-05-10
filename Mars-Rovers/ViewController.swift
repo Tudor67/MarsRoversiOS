e894db7c69a881736efaa1dd3d91f42beb9ad077
Tudor67 <buzu.tudor67@gmail.com>
2016-05-10 19:45:11 +0300

    Button Added

--- Mars-Rovers/ViewController.swift
+++ Mars-Rovers/ViewController.swift
@@ -9,17 +9,63 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
+    let btnX: CGFloat = 230
+    
+    let btnY: CGFloat = 40
+    
+    let btnWidth: CGFloat = 170
+    
+    let btnHeight: CGFloat = 35
+    
+    
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let button:UIButton = UIButton(frame: CGRectMake(btnX, btnY, btnWidth, btnHeight))
+        
+        button.backgroundColor = UIColor.greenColor()
+        
+        button.setTitle("Load Images", forState: UIControlState.Normal)
+        
+        button.addTarget(self, action: "buttonPressed:", forControlEvents: UIControlEvents.TouchUpInside)
+        
+        button.tag = 1;
+        
+        self.view.addSubview(button)
+        
     }
-
+    
+    
+    
+    func buttonPressed(sender:UIButton!) {
+        
+        let btnsendtag:UIButton = sender
+        
+        if btnsendtag.tag == 1 {
+            
+            print("Button pressed")
+            
+        }
+        
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
+        
         super.didReceiveMemoryWarning()
+        
         // Dispose of any resources that can be recreated.
+        
     }
-
-
+    
+    
+    
+    
+    
 }
-
